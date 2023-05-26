c4d66b636ba3682c041a1293ae4031ce23254894
eduardclan <eduardcala08@gmail.com>
2023-05-26 11:15:16 -0500

    modificaciones ghpages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: "/clase1402",
     server: {
         hmr: {
             host: 'localhost',
